962f7519cda063f31f76b8d77ff41fafe1256b8c
Lucas Barcelos <lukahbarcelos@gmail.com>
2023-08-03 15:30:46 -0300

    Desafio02 - Conversor de tipo númerico.

--- desafio02/desafio02.py
+++ desafio02/desafio02.py
@@ -6,7 +6,7 @@ selecao = 0
 
 print('')
 
-while selecao != 4:
+while selecao != "4":
     numero_usuario = int(input('Insira um número qualquer: '))
 
     selecao = input('Para converter seu número, escolha a tipo de Base que deseja:'+
@@ -15,20 +15,22 @@ while selecao != 4:
         '\n [3] - HEXADECIMAL'+
         '\n [4] - SAIR \n')
 
-    
-    if selecao == 1:
-        print("{0:b}".format(numero_usuario))
-
+    #Condição de escolha do usuário.
+    if selecao == "1":
+        print("O número {} convertido para BINÁRIO é: " .format(numero_usuario))
+        numero_convertido = bin(numero_usuario)
+        print(numero_convertido)
+        print("")
     elif selecao == "2":
-        numero_convertido = numero_usuario
-        oct(numero_convertido)
-        print('Seu número convertido para OCTAL é: ', numero_convertido)
-
+        print("O número {} convertido para OCTAL é: " .format(numero_usuario))
+        numero_convertido = oct(numero_usuario)
+        print(numero_convertido)
+        print("")
     elif selecao == "3":
-        numero_convertido = numero_usuario
-        hex(numero_convertido)
-        print('Seu número convertido para HEXADECIMAL é: ', numero_convertido)
-
+        print("O número {} convertido para HEXADECIMAL é: " .format(numero_usuario))
+        numero_convertido = hex(numero_usuario)
+        print(numero_convertido)
+        print("")
     elif selecao == "4":
         print('Obrigado por usar o Conversor!')
     else:
